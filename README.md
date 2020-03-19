ceed3678e5f9f27802b4f32870473ec76a909a4c
swetha123 <swetha.havalkod@gmail.com>
2020-03-19 11:47:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# physicsengine
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
---
+++ ground.js
@@ -0,0 +1,26 @@
+class Ground{
+    constructor(x,y,width,height){
+          var options1 = {
+             isStatic : true
+          }
+
+       this.body = Bodies.rectangle(x,y,width,height,options1);
+       this.width = width;
+       this.height = height;
+       World.add(world,this.body);
+
+       
+
+    }
+
+    display(){
+       var pos = this.body.position;
+       rectMode(CENTER);
+       fill(100);
+       rect(pos.x,pos.y,this.width,this.height);
+
+    }
+
+
+
+}
